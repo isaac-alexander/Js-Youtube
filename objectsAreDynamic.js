abc9f987c9788a571e112f0013d836fd1e68f7f3
Isaac Alexander <Isaacalexander81@gmail.com>
2025-02-22 02:16:47 +0100

    objectsAreDynamic.js

---
+++ objectsAreDynamic.js
@@ -0,0 +1,24 @@
+const person = {
+    name: 'Steven'
+};
+
+// console.log(person);
+
+person.favoriteFood = 'tacos';
+// console.log(person);
+
+person['favoriteIceCream'] = 'chocolate';
+
+delete person.favoriteIceCream;
+
+// console.log(person);
+
+person.eat = function() {
+    console.log('start eating');
+    
+}
+
+person.eat()
+
+
+// Explain how objects are dynamic Objects in JavaScript are dynamic because you can change and mutate the properties and methods of the object after ot has been initialized
