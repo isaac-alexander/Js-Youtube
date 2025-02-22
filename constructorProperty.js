d06af50ef6c8d9efaa518d8a04db1372d5679a5c
Isaac Alexander <Isaacalexander81@gmail.com>
2025-02-22 02:15:45 +0100

    constructorProperty.js

---
+++ constructorProperty.js
@@ -0,0 +1,14 @@
+// const person = {
+//     name: 'Steven'
+// };
+
+// console.log( person.constructor);
+
+// let newObj = {};
+// let newObj = new Object()
+
+new String(); // 'Steven'
+new Boolean(); // true, false
+new Number(); // 1, 2, 3
+
+// What is the constructor property? The constructor property is a property that exists for all objects in JavaScript. You can access it with either the dot notation or the bracket notation and it refrence the constructor function used to instantiate and create that object
