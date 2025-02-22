164e25c7cc8c15b4a77a43292b22a3d5530af9c5
Isaac Alexander <Isaacalexander81@gmail.com>
2025-02-22 02:16:21 +0100

    functionsAreObjects.js

---
+++ functionsAreObjects.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// const n = add;
+
+// console.log( n(2, 2));
+
+// console.log( add.length );
+
+// function Programmer(name) {
+//     this.name = name;
+//     this.writeCode = function() {
+//       console.log("Code in JavaScript");
+        
+//     }
+// }
+
+// console.log( Programmer.length);
+// console.log( Programmer.constructor);
+
+const Programmer = new Function('name',`
+  this.name = name;
+  this.writeCode = function() {
+  console.log("Code in JavaScript");
+  }
+    `);
+
+    const newProgrammer = new Programmer('Steven');
+
+    newProgrammer.writeCode()
+
+    // Explain how functions are objects in JavaScript? Internally fuctions are represented as objects in JavaScript, meaning that they have properties and methods.
