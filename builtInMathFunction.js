2a642bf4957ad63efe73abb02d6f8df4da04da9c
Isaac Alexander <Isaacalexander81@gmail.com>
2025-02-24 02:35:36 +0100

    builtInMathFunction.js

---
+++ builtInMathFunction.js
@@ -0,0 +1,20 @@
+let min = 1;
+let max = 10;
+
+const randomNum1 = Math.round( Math.random() * (max - min) + min);
+
+console.log(randomNum1);
+
+// Other built in methods include 
+// Math.floor()
+// Math.round()
+// Math.ceil()
+// Math.max()
+// Math.min()
+// Math.sqrt()
+// Math.pow()
+// Math.random()
+
+// how would you generate a random number between 1 and 100
+
+const randomNum = Math.round( Math.random() * (100 - 1) + 1);
