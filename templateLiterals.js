23366c8d586facad2092bdc274752fbed2f96144
Isaac Alexander <Isaacalexander81@gmail.com>
2025-02-25 01:25:00 +0100

    templateLiterals.js

---
+++ templateLiterals.js
@@ -0,0 +1,11 @@
+let firstName = 'Steven';
+let lastName = 'Gracia';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+let fullName2 = `${firstName} ${lastName}`
+console.log(fullName2);
+
+//Template Litertals
+// What are template literals Template literals provides an alternative syntax to the traditional strings by using back ticks. The benefit is it enables us to use string interpolation, meaning that with the dollar sign and then curly braces we can utilize that as a placeholder and sspecify Expressions within it rather than having to use concatenation with the plus operator.
