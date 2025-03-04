c84899d716a14200e873214d038344157040b72b
Isaac Alexander <Isaacalexander81@gmail.com>
2025-03-04 02:07:31 +0100

    spreadOperator.js

---
+++ spreadOperator.js
@@ -0,0 +1,7 @@
+const exampleNumbersA = [1, 2, 3];
+const exampleNumbersB = [4, 5, 6];
+
+let combined = [...exampleNumbersA, ...exampleNumbersB];
+console.log(combined);
+
+// What is the spread operator? The  spread operator refers to using 3 dots... in order to copy over an object, so this can refer to an object literal and copying over the properties it could also refer to the elements of an array
